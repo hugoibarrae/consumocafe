73c12079f4683806f490ef27185289539b918bf9
hugoibarrae <72998850+hugoibarrae@users.noreply.github.com>
2026-05-03 15:18:05 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+function onScanSuccess(decodedText, decodedResult) {
+    // Se ejecuta cuando se detecta un código
+    document.getElementById('result').innerHTML = `
+        <p>Contenido detectado:</p>
+        <a href="${decodedText}" target="_blank">${decodedText}</a>
+    `;
+    
+    // Opcional: Detener el escaneo tras el éxito
+    // html5QrcodeScanner.clear();
+}
+
+function onScanFailure(error) {
+    // Este callback se ejecuta en cada frame si no encuentra un QR
+    // Se deja vacío para no saturar la consola
+}
+
+// Configuración del escáner
+let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader", 
+    { fps: 10, qrbox: {width: 250, height: 250} },
+    /* verbose= */ false
+);
