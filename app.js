c43552747ca1133b35c6ddd589121c380e32f923
hugoibarrae <72998850+hugoibarrae@users.noreply.github.com>
2026-05-03 15:21:14 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -20,3 +20,5 @@ let html5QrcodeScanner = new Html5QrcodeScanner(
     { fps: 10, qrbox: {width: 250, height: 250} },
     /* verbose= */ false
 );
+
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
