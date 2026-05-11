b712ef008c562bf1f0c8e64f4c3c702ba64774bc
hugoibarrae <72998850+hugoibarrae@users.noreply.github.com>
2026-05-11 12:04:48 -0600

    Add files via upload

--- appcopia3.js
+++ appcopia3.js
@@ -39,7 +39,7 @@ function onScanSuccess(decodedText) {
     btnEnviar.disabled = false;
     btnEnviar.innerText = "Validar y Canjear";
     btnEnviar.style.backgroundColor = "#2e7d32";
-    btnEnviar.style.color = "orange";
+    btnEnviar.style.color = "white";
     btnEnviar.style.cursor = "pointer";
 }
 
