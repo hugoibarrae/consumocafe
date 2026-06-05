36c27a354ff7cb218c0987a2c37ea244daecc046
hugoibarrae <72998850+hugoibarrae@users.noreply.github.com>
2026-06-04 23:28:02 -0600

    Add files via upload

--- buscar.js
+++ buscar.js
@@ -1,7 +1,7 @@
 // --- CONFIGURACIÓN ---
 // REEMPLAZA CON TU URL DE EJECUCIÓN NUEVA DE APPS SCRIPT
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzqcLOaCFHnA5Diz6Dy7dYnv13_RDPWWYfuyJugP_aMkGSoqSfjFJKmxQAwe-f4_fX2/exec'; 
-
+    
 
 const selectArea = document.getElementById('select-area');
 const txtBuscar = document.getElementById('txt-buscar');
